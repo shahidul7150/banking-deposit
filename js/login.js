065089e5088db34c8bd82a4c653d3c4081b4d46e
Shahidul Islam <shahidulislam7150@gmail.com>
2022-02-10 23:05:19 +0600

    banking add

---
+++ js/login.js
@@ -0,0 +1,13 @@
+document.getElementById('login-submit').addEventListener('click',function(){
+    const emailField=document.getElementById('user-email');
+    const userEmail=emailField.value;
+    
+    // get user password
+    const passwordField=document.getElementById('user-password');
+    const userPassword=passwordField.value ;
+    if(userEmail=='bangla@gmail.com' && userPassword=='bangla'){
+        window.location.href='banking.html'
+    }
+
+
+})
